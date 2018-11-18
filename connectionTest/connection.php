ed83b4ce9b7e89015179d9b4c90a655ab0cc961b
Felipe Negri <felipe.negri43@gmail.com>
2018-11-18 11:48:20 -0200

    2

--- connectionTest/connection.php
+++ connectionTest/connection.php
@@ -7,14 +7,15 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-
-$query = 'INSERT INTO CONTATO(NOME, EMAIL, ASSUNTO, MENSAGEM) VALUES ("jose"," manuel@gmail.com", "dfsdf", "sdfsdfd")';
-
-if($link->query($query) === TRUE){
-    echo"INSERIU"
-}
-
 echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+$query = "INSERT INTO CONTATO(NOME, EMAIL, ASSUNTO, MENSAGEM) VALUES ('jose','manuel@gmail.com', 'dfsdf', 'sdfsdfd')";
+
+if($link->query($query) === TRUE) {
+    echo"INSERIU"
+} else {
+    echo "Error: " . $query . "<br>" . $link->error;
+}
 
 mysqli_close($link);
