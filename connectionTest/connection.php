390fba5013a5ac703bb12a10ad92bd93c351eb59
Felipe Negri <felipe.negri43@gmail.com>
2018-11-18 11:54:52 -0200

    Agora vai

--- connectionTest/connection.php
+++ connectionTest/connection.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("den1.mysql2.gear.host", "gameland", "Vf62202_4!Or");
+$link = mysqli_connect("den1.mysql2.gear.host", "gameland", "Vf62202_4!Or","gameland");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -7,15 +7,17 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+if($link){
+    echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+}
 
-$query = "INSERT INTO CONTATO(NOME, EMAIL, ASSUNTO, MENSAGEM) VALUES ('jose','manuel@gmail.com', 'dfsdf', 'sdfsdfd')";
+$sql = "INSERT INTO CONTATO (NOME, EMAIL, ASSUNTO, MENSAGEM) VALUES ('jose','manuel@gmail.com', 'dfsdf', 'sdfsdfd')";
 
-if($link->query($query) === TRUE) {
-    echo"INSERIU"
-} else {
-    echo "Error: " . $query . "<br>" . $link->error;
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
 mysqli_close($link);
