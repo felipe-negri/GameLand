a5f762f5f6c2acc9e99761d488dadb6401ff8469
Felipe Negri <felipe.negri43@gmail.com>
2018-11-18 11:24:48 -0200

    1

--- connectionTest/connection.php
+++ connectionTest/connection.php
@@ -1,16 +1,16 @@
 <?php
-$con=mysqli_connect("den1.mysql2.gear.host", "gameland", "Vf62202_4!Or","gameland");
-echo('CONECTADO')
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$link = mysqli_connect("den1.mysql2.gear.host", "gameland", "Vf62202_4!Or");
 
-// Perform queries
-mysqli_query($con,"SELECT * FROM Persons");
-mysqli_query($con,"INSERT INTO Persons (FirstName,LastName,Age)
-VALUES ('Glenn','Quagmire',33)");
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
-mysqli_close($con);
-?> 
+mysqli_query($link,"INSERT INTO CONTATO(NOME, EMAIL, ASSUNTO, MENSAGEM) VALUES (jose, manuel@gmail.com, dfsdf, sdfsdfd)");
+
+echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
